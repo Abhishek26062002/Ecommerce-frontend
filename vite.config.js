3ffa85125d5621507ff29c0aec9ddd2fbd378a70
Abhishek26062002 <abhishekpeddada26@gmail.com>
2025-12-09 11:03:57 +0530

    Add base option to Vite configsave
    Added base option to Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -16,4 +7,3 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: "/",   // <-- Add this
 });
->>>>>>> 90c42162987c4e3cea6a537cf47863f0739dc0df
